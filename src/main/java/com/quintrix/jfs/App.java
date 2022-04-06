21a2e7e1746880fe5998d9153ce4f051a903207a
MarkNaSSIB <markblankboutros@gmail.com>
2022-04-05 20:10:22 -0400

    On branch main  Your branch is up to date with 'origin/main'. initial code commit

---
+++ src/main/java/com/quintrix/jfs/App.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs;
+
+/**
+ * Mark Boutros - Hello World
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Will Print Hello World
+        System.out.println( "Hello World!" );
+    }
+}
